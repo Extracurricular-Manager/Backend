cf9e1a28376c38a72658ea4448f2446a3188454f
BenjaminNavet <benjamin.navet@live.fr>
2022-02-02 17:51:01 +0100

    ajout dependences mapstruct

--- src/main/java/fr/periscol/backend/BackendApplication.java
+++ src/main/java/fr/periscol/backend/BackendApplication.java
@@ -1,11 +1,11 @@
 package fr.periscol.backend;
 
-import fr.periscol.backend.security.jwt.JHipsterProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(JHipsterProperties.class)
+
 @SpringBootApplication
 public class BackendApplication {
 
