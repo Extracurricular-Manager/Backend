e6a7a0c02dd39407dd702e9684e8a3eb9db323f3
Théo <theo.giraudet@orange.fr>
2022-02-10 14:24:42 +0100

    Update API Title

--- src/main/java/fr/periscol/backend/config/SwaggerConfiguration.java
+++ src/main/java/fr/periscol/backend/config/SwaggerConfiguration.java
@@ -1,5 +1,7 @@
 package fr.periscol.backend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -11,33 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Component
 public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 
-    /*private final String description;
-
     @Bean
-    public OpenAPI customOpenAPI(@Value("${minecraft-data.api-version}") String appVersion) {
+    public OpenAPI customOpenAPI(@Value("${periscol.api-version}") String appVersion) {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Minecraft Resource API")
+                        .title("Periscol API")
                         .version(appVersion)
-                        .description(description)
-
                 );
     }
-
-    public SwaggerConfiguration() {
-        final var opt = Utils.readResourceFile("description");
-        if (opt.isEmpty())
-            throw new NullPointerException("'description' file doesn't exist in resources");
-        description = opt.get();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    /*
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-    */
 }
