51babde50217a2813ca3481a4a6bf772f0014c5d
BenjaminNavet <benjamin.navet@live.fr>
2022-02-27 20:49:13 +0100

    add API get Monthpaid by child and by child/date

--- src/main/java/fr/periscol/backend/repository/MonthPaidRepository.java
+++ src/main/java/fr/periscol/backend/repository/MonthPaidRepository.java
@@ -1,12 +1,30 @@
 package fr.periscol.backend.repository;
 
 import fr.periscol.backend.domain.MonthPaid;
+import fr.periscol.backend.domain.service_model.PeriodModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data SQL repository for the Facturation entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface MonthPaidRepository extends JpaRepository<MonthPaid, Long> {}
+public interface MonthPaidRepository extends JpaRepository<MonthPaid, Long> {
+
+
+    @Query("select m from MonthPaid m where m.child.id = :idChild AND m.date = :date")
+    Optional<MonthPaid> findOneByChildAndDate(Long idChild, LocalDateTime date);
+
+    @Query("select m from MonthPaid m where m.child.id = :idChild")
+    Optional<MonthPaid> findAllByChild(Long idChild);
+
+}
--- src/main/java/fr/periscol/backend/service/MonthPaidService.java
+++ src/main/java/fr/periscol/backend/service/MonthPaidService.java
@@ -8,6 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -32,69 +35,107 @@ public class MonthPaidService {
     }
 
     /**
-     * Save a facturation.
+     * Save a monthPaid.
      *
      * @param monthPaidDTO the entity to save.
      * @return the persisted entity.
      */
     public MonthPaidDTO save(MonthPaidDTO monthPaidDTO) {
-        log.debug("Request to save Facturation : {}", monthPaidDTO);
+        log.debug("Request to save MonthPaid : {}", monthPaidDTO);
         fr.periscol.backend.domain.MonthPaid monthPaid = monthPaidMapper.toEntity(monthPaidDTO);
         monthPaid = monthPaidRepository.save(monthPaid);
         return monthPaidMapper.toDto(monthPaid);
     }
 
     /**
-     * Partially update a facturation.
+     * Partially update a monthPaid.
      *
      * @param monthPaidDTO the entity to update partially.
      * @return the persisted entity.
      */
     public Optional<MonthPaidDTO> partialUpdate(MonthPaidDTO monthPaidDTO) {
-        log.debug("Request to partially update Facturation : {}", monthPaidDTO);
+        log.debug("Request to partially update MonthPaid : {}", monthPaidDTO);
 
         return monthPaidRepository
             .findById(monthPaidDTO.getId())
-            .map(existingFacturation -> {
-                monthPaidMapper.partialUpdate(existingFacturation, monthPaidDTO);
+            .map(existingMonthPaid -> {
+                monthPaidMapper.partialUpdate(existingMonthPaid, monthPaidDTO);
 
-                return existingFacturation;
+                return existingMonthPaid;
             })
             .map(monthPaidRepository::save)
             .map(monthPaidMapper::toDto);
     }
 
     /**
-     * Get all the facturations.
+     * Get the child monthPaids by date.
+     *
+     * @return the one entities.
+     */
+    @Transactional(readOnly = true)
+    public Optional<MonthPaidDTO> findOneByChildAndDate(Long childId, Date date) {
+        log.debug("Request to get the child MonthPaid at a month");
+        return monthPaidRepository.findOneByChildAndDate(childId, toLDT(date)).map(monthPaidMapper::toDto);
+    }
+
+
+    /**
+     * Get all the monthPaids of a child.
+     *
+     * @return the list of entities.
+     */
+    @Transactional(readOnly = true)
+    public List<MonthPaidDTO> findAllByChild(Long childId) {
+        log.debug("Request to get all the child MonthPaid");
+        return monthPaidRepository.findAllByChild(childId).stream()
+                .map(monthPaidMapper::toDto)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    /**
+     * Get all the monthPaids.
      *
      * @return the list of entities.
      */
     @Transactional(readOnly = true)
     public List<MonthPaidDTO> findAll() {
-        log.debug("Request to get all Facturations");
-        return monthPaidRepository.findAll().stream().map(monthPaidMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+        log.debug("Request to get all MonthPaid");
+        return monthPaidRepository.findAll().stream()
+                .map(monthPaidMapper::toDto)
+                .collect(Collectors.toCollection(LinkedList::new));
     }
 
+
+
     /**
-     * Get one facturation by id.
+     * Get one monthPaid by id.
      *
      * @param id the id of the entity.
      * @return the entity.
      */
     @Transactional(readOnly = true)
     public Optional<MonthPaidDTO> findOne(Long id) {
-        log.debug("Request to get Facturation : {}", id);
+        log.debug("Request to get MonthPaid : {}", id);
         return monthPaidRepository.findById(id).map(monthPaidMapper::toDto);
     }
 
+
     /**
-     * Delete the facturation by id.
+     * Delete the monthPaid by id.
      *
      * @param id the id of the entity.
      */
     public void delete(Long id) {
-        log.debug("Request to delete Facturation : {}", id);
+        log.debug("Request to delete MonthPaid : {}", id);
         monthPaidRepository.deleteById(id);
     }
 
+    /**
+     * Convert a {@link Date} to a {@link LocalDateTime}
+     * @param date a Date ton convert
+     * @return the LocalDateTime
+     */
+    private LocalDateTime toLDT(Date date) {
+        return new Timestamp(date.getTime()).toLocalDateTime().withHour(0).withMinute(0).withSecond(0).withNano(0);
+    }
 }
