5fe9b1e57bb45fbc2d459420f12db3bd8e02c947
Théo <theo.giraudet@orange.fr>
2022-02-28 17:14:54 +0100

    Modify CriteriaType

--- src/main/java/fr/periscol/backend/domain/tarification/CriteriaType.java
+++ src/main/java/fr/periscol/backend/domain/tarification/CriteriaType.java
@@ -8,6 +8,8 @@ import fr.periscol.backend.domain.tarification.child.CriteriaChildBasePeriod;
 import fr.periscol.backend.domain.tarification.child.CriteriaChildBasePresence;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public enum CriteriaType {
@@ -18,8 +20,42 @@ public enum CriteriaType {
     BASE_CRITERIA_PRESENCE(4, "Tarif de base cantine", CriteriaChildBasePresence::new, ModelEnum.PRESENCE)
     ;
 
-    CriteriaType(int i, String s, Supplier<? extends Criteria> aNew, ModelEnum period) {
+    private final int id;
+    private final String name;
+    private final Supplier<? extends Criteria> constructor;
+    private final ModelEnum model;
+
+    CriteriaType(int id, String name, Supplier<? extends Criteria> constructor, ModelEnum model) {
+        this.id = id;
+        this.name = name;
+        this.constructor = constructor;
+        this.model = model;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public Supplier<? extends Criteria> getConstructor() {
+        return constructor;
+    }
+
+    public ModelEnum getModel() {
+        return model;
+    }
+
+    public static Collection<CriteriaType> getFromModelType(ModelEnum model) {
+        return Arrays.stream(CriteriaType.values()).filter(m -> m.model == model).toList();
+    }
+
+    public static Optional<Criteria> getFromId(int id) {
+        return Arrays.stream(CriteriaType.values())
+                .filter(m -> m.id == id)
+                .findFirst()
+                .map(c -> c.constructor.get());
+    }
 }
