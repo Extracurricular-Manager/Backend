d9e0d93b41472cdcf2f0a23146f8178703a4a696
albgut <alban.gutierrez-andre@etudiant.univ-rennes1.fr>
2022-03-03 19:33:46 +0100

    clean export

--- src/main/java/fr/periscol/backend/domain/csvUtil/XlsxUtil.java
+++ src/main/java/fr/periscol/backend/domain/csvUtil/XlsxUtil.java
@@ -1,12 +1,10 @@
 package fr.periscol.backend.domain.csvUtil;
 
-import com.opencsv.CSVWriter;
 import fr.periscol.backend.domain.Child;
 import fr.periscol.backend.domain.service_model.ServiceMetadata;
 import fr.periscol.backend.service.BillingCalculationService;
 import fr.periscol.backend.service.MonthPaidService;
 import fr.periscol.backend.service.dto.ChildDTO;
-import fr.periscol.backend.service.dto.service_model.ServiceMetadataDTO;
 import fr.periscol.backend.service.mapper.ChildMapper;
 import fr.periscol.backend.service.mapper.service_model.ServiceMetadataMapper;
 import fr.periscol.backend.service.service_model.PeriodModelService;
@@ -15,12 +13,9 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.time.Month;
-import java.time.YearMonth;
 import java.util.*;
 
-public class CsvUtil {
+public class XlsxUtil {
 
     private final MonthPaidService monthPaidService;
     private final ChildMapper childMapper;
@@ -28,7 +23,7 @@ public class CsvUtil {
     private final ServiceMetadataService serviceMetadataService;
     private final ServiceMetadataMapper serviceMetadataMapper;
 
-    public CsvUtil(MonthPaidService monthPaidService, ChildMapper childMapper, PeriodModelService periodService, ServiceMetadataService serviceMetadata, ServiceMetadataMapper serviceMetadataMapper){
+    public XlsxUtil(MonthPaidService monthPaidService, ChildMapper childMapper, PeriodModelService periodService, ServiceMetadataService serviceMetadata, ServiceMetadataMapper serviceMetadataMapper){
         this.monthPaidService = monthPaidService;
         this.childMapper = childMapper;
         this.periodService = periodService;
@@ -36,26 +31,6 @@ public class CsvUtil {
         this.serviceMetadataMapper = serviceMetadataMapper;
     }
 
-    public Map<Date, ServiceMetadataDTO> getFromChild(ChildDTO childDTO){
-        HashMap<Date, ServiceMetadataDTO> dateServiceResourceHashMap = new HashMap<>();
-        dateServiceResourceHashMap.put(new Date(2022, 12, 12), new ServiceMetadataDTO());
-        return dateServiceResourceHashMap;
-    }
-
-    public File csvWriter(List<String[]> stringArray, Path path) throws Exception {
-        CSVWriter writer = new CSVWriter(new FileWriter(path.toString()));
-        for (String[] array : stringArray) {
-            writer.writeNext(array);
-        }
-        writer.close();
-        return new File(path.toString());
-    }
-
-    public File buildCsv(){
-        File file = new File("test.csv");
-
-        return file;
-    }
 
     public Workbook createChildSheet(Workbook workbook, ChildDTO childDTO, Date currentMonthAndYear){
         Sheet childSheet = workbook.createSheet(childDTO.getSurname().toUpperCase() +  " " + childDTO.getName() );
@@ -65,17 +40,8 @@ public class CsvUtil {
         for (int i = 1 ; i <= serviceMetadataList.size() ; i++){
             mainRow.createCell(i).setCellValue(serviceMetadataList.get(i - 1).getName().toUpperCase());
         }
-        //Calendar. TODO make calendar....
         Calendar calendar = Calendar.getInstance();
-        /*
-        YearMonth ym = YearMonth.of( 2017 , Month.MARCH );
-        ym.atDay(1);
-         */
-        /*
-        calendar.set(Calendar.MONTH, currentMonthAndYear.getMonth());
-        calendar.set(Calendar.YEAR, currentMonthAndYear.getYear());
 
-         */
         calendar.setTime(currentMonthAndYear);
         calendar.set(Calendar.DAY_OF_MONTH, 1);
 
@@ -86,14 +52,6 @@ public class CsvUtil {
             dates.add(calendar.getTime());
             calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
-
-        /*
-        //TODO test to remove
-        for (int i = 0 ; i < 30 ; i++){
-            dates.add(new Date(i * 2000000000));
-        }
-        //end test to remove
-         */
         BillingCalculationService billingCalculationService = new BillingCalculationService(periodService);
         for (int i = 1 ; i <= dates.size() ; i++){
             Row row = childSheet.createRow(i);
@@ -110,43 +68,16 @@ public class CsvUtil {
                 row.createCell(j).setCellValue(bcs);
             }
         }
-        //TODO make calculation for the total last line.
-        //TODO clean Empty column + empty row.
-        //Generate a row with DATE -> cost for each Service -> Total for the date
-        /*
-        Row shinyExample = childSheet.createRow(1);
-        shinyExample.createCell(0).setCellValue("01-01-2001");
-        shinyExample.createCell(1).setCellValue(2.0);
-        shinyExample.createCell(2).setCellValue(5.0);
-         */
+        for (int i = 0 ; i <= serviceMetadataList.size() ; i++){
+            childSheet.autoSizeColumn(i);
+        }
         return workbook;
     }
 
     public File createXlsx(Date date, ChildDTO childDTO) throws IOException {
 
         Workbook workbook = new XSSFWorkbook();
-        /*
-        ChildDTO childDTO = new ChildDTO();
-        childDTO.setName("Bilbo");
-        childDTO.setSurname("Squat");
-         */
         workbook = createChildSheet(workbook, childDTO, date);
-
-        /*
-        Sheet sheet2 = workbook.createSheet("test_2");
-
-        Row row = sheet2.createRow(0);
-
-        Cell headerCell = row.createCell(0);
-        headerCell.setCellValue("Coucou");
-
-        headerCell = row.createCell(1);
-        headerCell.setCellValue("Le");
-
-        headerCell = row.createCell(2);
-        headerCell.setCellValue("Monde");
-
-         */
 
         File currDir = new File(".");
         String path = currDir.getAbsolutePath();
@@ -159,15 +90,5 @@ public class CsvUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        /*
-        String[] array0 = new String[3];
-        String[] array1 = new String[3];
-        array0[0] = "COL_1"; array0[1] = "COL_2"; array0[2] = "COL_3";
-        array1[0] = "test1"; array1[1] = "test2"; array1[2] = "test3";
-        List<String[]> list = new ArrayList<>();
-        list.add(array0);list.add(array1);
-        File file = csvWriter(list, Path.of("test.csv"));
-        file.createNewFile();
-         */
     }
 }
