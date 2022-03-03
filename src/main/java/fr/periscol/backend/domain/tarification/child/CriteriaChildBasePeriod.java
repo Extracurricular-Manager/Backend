12942dafa46a4e2a797461723f6b59b842b279f6
albgut <alban.gutierrez-andre@etudiant.univ-rennes1.fr>
2022-03-03 18:34:49 +0100

    criteria base period refactor

--- src/main/java/fr/periscol/backend/domain/tarification/child/CriteriaChildBasePeriod.java
+++ src/main/java/fr/periscol/backend/domain/tarification/child/CriteriaChildBasePeriod.java
@@ -4,14 +4,15 @@ import fr.periscol.backend.domain.Child;
 import fr.periscol.backend.domain.tarification.AttributeType;
 import fr.periscol.backend.domain.tarification.Attributes;
 import fr.periscol.backend.domain.tarification.CriteriaChild;
-import fr.periscol.backend.service.dto.service_model.PeriodModelDTO;
 import fr.periscol.backend.service.service_model.PeriodModelService;
 
 import javax.persistence.Entity;
 
 import static fr.periscol.backend.domain.tarification.AttributeType.*;
 
-import java.time.LocalDateTime;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 /**
@@ -27,8 +28,8 @@ public class CriteriaChildBasePeriod extends CriteriaChild<PeriodModelService> {
     public CriteriaChildBasePeriod(){
 
 
-        String name[] = {"price", "time","maxBound", "minBound"};
-        AttributeType type[] = {CURRENCY, INT, TIME, TIME};
+        String[] name = {"price", "time","maxBound", "minBound"};
+        AttributeType[] type = {CURRENCY, INT, TIME, TIME};
 //        Map<Integer, Set<Object>> value = Map.of(
 //                1, Set.of("price", 0.5f),
 //                2, Set.of("time", 15),
@@ -36,8 +37,8 @@ public class CriteriaChildBasePeriod extends CriteriaChild<PeriodModelService> {
 //                4, Set.of("minbound", new Time(7,0,0))
 //        );
 
-        String value[] = {"0.5", "15","23:59","00:00"};
-        String description[] = {
+        String[] value = {"0.5", "15","23:59","00:00"};
+        String[] description = {
                 "price of the prestation during regular hours",
                 "time scale of the price",
                 "End time of daycare",
@@ -51,15 +52,21 @@ public class CriteriaChildBasePeriod extends CriteriaChild<PeriodModelService> {
 
     @Override
     public float compute(Child child, Long serviceId, Date date, float price, PeriodModelService dataService) {
-        Optional<PeriodModelDTO> periodModelDTOOptional = dataService.findOneForDay(child.getId(), serviceId, date);
-        if (periodModelDTOOptional.isPresent()){
-            LocalDateTime timeOfArrival = periodModelDTOOptional.get().getTimeOfArrival();
-            LocalDateTime timeOfDeparture = periodModelDTOOptional.get().getTimeOfDeparture();
-            int hours = timeOfDeparture.getHour() - timeOfArrival.getHour();
-            int numberOfQuarters = (timeOfDeparture.getMinute() - timeOfArrival.getMinute() + hours * 60) / 15;
-            //TODO REMOVE For test just one attribute
-            return Float.parseFloat(this.attributes.get(0).getValue()) * numberOfQuarters;
-        }
-        return price;
+        final var base = Float.parseFloat(attributes.get(0).getValue());
+        final var maxBound = LocalTime.parse(attributes.get(2).getValue());
+        final var range = Integer.parseInt(attributes.get(1).getValue());
+
+        final var entry = dataService.findOneForDay(child.getId(), serviceId, date);
+        if(entry.isEmpty())
+            return price;
+
+        final var startTime = entry.get().getTimeOfStartBilling().toLocalTime();
+        var endTime = entry.get().getTimeOfDeparture().toLocalTime();
+
+        endTime = endTime.isAfter(maxBound) ? maxBound : endTime;
+
+        final var duration = Duration.between(startTime, endTime).get(ChronoUnit.SECONDS);
+        final long durationInMinutes = duration / 60;
+        return price + base * (durationInMinutes / range);
     }
 }
